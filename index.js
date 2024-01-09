715e66a365a63b218360e6c4232b73dacc1a5c34
Arnold-In-Tech <g16a7782@gmail.com>
2024-01-09 03:59:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+let name = "Steve"
+let email = "steve@steve.com"
+
+function submitData(usrName, usrEmail){
+
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+                     name : `${usrName}`,
+                     email: `${usrEmail}`
+                 })
+      })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        let body = document.querySelector('body');
+        body.innerHTML = object.id;        
+      })
+      .catch(function (error) {
+        let body = document.querySelector('body');
+        body.innerHTML = error.message;
+      })
+}
+
+submitData(name, email)
